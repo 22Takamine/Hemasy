4f45f0167e447baa2e30f153358edc015f6d2586
TTK2039 <87115512+TTK2039@users.noreply.github.com>
2022-06-20 14:41:40 +0900

    年次と月次追加

--- memo.sql
+++ memo.sql
@@ -2,8 +2,8 @@
 select T1.lists_and_records_id ID
 , T1.value1 運動の種類
 , T1.value2 Mets
-, T1.value3 運動時間\/分
-, T1.value3/60 運動時間\/時間
+, T1.value3 運動時間
+, T1.value3/60 運動時間
 , T1.create_date 日付
 , T1.user_id ユーザーID
 , T2.value2 体重
@@ -16,9 +16,6 @@ AND T1.category = 2
 AND T2.category = 2
 AND T1.type = 2
 AND T2.type = 5;
-
--- WHERE T1.user_id = ?
-;
 
 -- 運動記録
 
@@ -35,3 +32,24 @@ lists_and_records
 -- メインページで使う　統計に遷移する際に送るデータをとるsql
 select  sum(value2*value3) calocie, create_date 日付 from lists_and_records  where category = 2 and type = 1 group by create_date;
 
+-- 月でくくって日ごとの消費カロリーを計算します
+select create_date AS day
+, sum(value2*value3) 摂取カロリー
+ from lists_and_records 
+ where left(to_char(create_date, 'YYYY-MM'), 7) = left('2022-06-23', 7)
+ AND user_id = 1
+ AND category = 2
+ AND type = 1
+ group by create_date
+ ORDER BY day;
+
+-- 年でくくって月ごとの消費カロリー計算
+SELECT to_char(create_date, 'YYYY-MM') AS month,
+    sum(value2 * value3) 摂取カロリー
+FROM lists_and_records
+where category =2
+AND type = 1
+AND user_id = 1
+AND left(to_char(create_date, 'YYYY-MM'), 4) = left('2022-04-23', 4)
+GROUP BY month
+ORDER BY month;
