278cae6f8191be540dc7a90998af424738bb626a
Takamine Yusuke <22takamine_yuusuke@axiz.co.jp>
2022-06-21 09:46:28 +0900

    registerForm追加

--- Hemasy-1/src/main/java/com/example/form/registerForm.java
+++ Hemasy-1/src/main/java/com/example/form/registerForm.java
@@ -6,11 +6,40 @@ import javax.validation.constraints.NotEmpty;
 public class registerForm {
     
     @NotEmpty
+    private String name;
+    
+    @NotEmpty
     private String mail;
     
     @NotEmpty
     private String pass;
+    
+    @NotEmpty
+    private String sex;
+    
+    @NotEmpty
+    private String birthDate;
+    
+    @NotEmpty
+    private String height;
+    
+    @NotEmpty
+    private String weight;
+    
+    private String bodyFat;
+    private String rank;
+    private String smoke;
+    private String alcohol;
 
+    	
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
     public String getMail() {
         return mail;
     }
@@ -26,5 +55,71 @@ public class registerForm {
     public void setPass(String pass) {
         this.pass = pass;
     }
+    
+    public String getSex() {
+        return sex;
+    }
 
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+    
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+    
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+    
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+    
+    public String getBodyFat() {
+        return bodyFat;
+    }
+
+    public void setBodyFat(String bodyFat) {
+        this.bodyFat = bodyFat;
+    }
+    
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+    
+    public String getsmoke() {
+        return smoke;
+    }
+
+    public void setSmoke(String smoke) {
+        this.smoke = smoke;
+    }
+    
+    public String getAlcohol() {
+        return alcohol;
+    }
+
+    public void setAlcohol(String alcohol) {
+        this.alcohol = alcohol;
+    }
+    
+    
+    
 }
---
+++ Hemasy-1/src/main/webapp/WEB-INF/views/account.jsp
@@ -0,0 +1,36 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+  pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>商品詳細</title>
+<link href="css/commons.css" rel="stylesheet">
+</head>
+<body>
+<header>
+	<div class="header-logo"><a href="result">Hemasy</a></div>
+	<form:form action="hamburger" modelAttribute="index" method="post">
+	    <button type="button" class="menu-btn">
+	      <i class="fa fa-bars" aria-hidden="true"></i>
+	    </button>
+	    <div class="menu">
+	      <div class="menu__item"><a href="./account">アカウント管理</a></div>
+	      <div class="menu__item"><a href="./rank">ランキング</a></div>
+	      <div class="menu__item"><a href="./list">リスト編集</a></div>
+	      <div class="menu__item"><a href="./information">お問い合わせ</a></div>
+	      <div class="menu__item"><a href="./logout">ログアウト</a></div>
+	    </div>
+    </form:form>
+</header>
+
+  <hr>
+	<label><fmt:message key="form.lbl.mail"/></label><form:input path="mail" type="text"/>
+    <form:errors path="mail" cssStyle="color: red"/>
+
+</body>
+</html>
