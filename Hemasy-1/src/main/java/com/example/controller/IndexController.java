18c99c4fe3f43fcf63746775c10bddff6fa2ad2b
Takamine Yusuke <22takamine_yuusuke@axiz.co.jp>
2022-06-21 16:15:47 +0900

    開発環境

--- Hemasy-1/src/main/java/com/example/controller/IndexController.java
+++ Hemasy-1/src/main/java/com/example/controller/IndexController.java
@@ -1,7 +1,5 @@
 package com.example.controller;
 
-import java.util.Locale;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,30 +12,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.example.dao.UserDao;
-import com.example.entity.Login;
-import com.example.entity.Users;
 import com.example.form.IndexForm;
 import com.example.form.registerForm;
-import com.example.service.LoginService;
 
 
 
 @Controller
 public class IndexController {
 
-//    @Autowired
-//    ProductService productService;
-//    
-	
-	@Autowired
-	LoginService loginService;
-
     @Autowired
     MessageSource messageSource;
-    
-    @Autowired
-	UserDao userDao;
     
     @Autowired
 	HttpSession session; 
@@ -53,36 +37,8 @@ public class IndexController {
     	if (bindingResult.hasErrors()) {	
             return "login";
         }
-
-		Login user = loginService.findIdAndPass(form.getMail(),form.getPass());
-		
-		if (user == null) {
-			// メッセージリソースファイルから、メッセージを取得
-			String errMsg = messageSource.getMessage("select.error", null, Locale.getDefault());
-			model.addAttribute("msg", errMsg);
-		
-			return "login";
-			
-		} else {
-			//確認用
-			System.out.println(user);
-			//sessionに保存
-			session.setAttribute("user", user);
-			//保存されているかの確認
-			System.out.println(session.getAttribute("user"));
-			
-			//BMI計算をするためにuser_idを取得する
-			var user_id = user.getUser_id();
-			
-			//・身長と体重をもとにBMIを算出するDAOを呼び出す。
-			Users bmi = null;
-			
-			bmi = userDao.BMI(user_id);
-			
-			System.out.println(bmi.getBmi());
-			
-			return "menu";
-		}
+    	
+    	return "menu";
     	
     }
     
